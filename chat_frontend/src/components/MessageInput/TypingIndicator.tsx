8997d6e76f3f0d1fd8fb81041455776a29d619b9
Pavel Grebnev <pavel.grebnev@rocknblock.io>
2025-06-10 23:48:54 +0300

    add typing indicator new dot styles

--- chat_frontend/src/components/MessageInput/TypingIndicator.tsx
+++ chat_frontend/src/components/MessageInput/TypingIndicator.tsx
@@ -9,7 +9,12 @@ interface TypingIndicatorProps {
 const TypingIndicator: React.FC<TypingIndicatorProps> = ({show, isOperator}) =>
   show ? (
     <div className={styles.typingIndicator}>
-      {isOperator ? "User is typing..." : "Operator is typing..."}
+      {isOperator ? "User is typing" : "Operator is typing"}
+      <span className={styles.dots}>
+    <span className={styles.dot}></span>
+    <span className={styles.dot}></span>
+    <span className={styles.dot}></span>
+  </span>
     </div>
   ) : null;
 
