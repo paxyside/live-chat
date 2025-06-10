630a273d2add8ab1da1ab1f55c6671b74b2bef0d
Pavel Grebnev <pavel.grebnev@rocknblock.io>
2025-06-11 01:09:54 +0300

    udate drpdwn menu

--- chat_frontend/src/components/Message/MessageDropdownMenu.tsx
+++ chat_frontend/src/components/Message/MessageDropdownMenu.tsx
@@ -1,22 +1,45 @@
 import React, {useRef} from "react";
 import {usePopupClose} from "@/components/Message/hooks/usePopupClose.ts";
-import styles from './Message.module.css';
+import styles from "./Message.module.css";
+import {Pencil, Trash2} from "lucide-react";
 
 interface Props {
-  onConfirmEdit: () => void
+  onConfirmEdit: () => void;
   onConfirmDelete: () => void;
   onCancel: () => void;
 }
 
-const MessageDropDownMenu: React.FC<Props> = ({onConfirmDelete, onCancel, onConfirmEdit}) => {
+const MessageDropDownMenu: React.FC<Props> = ({
+                                                onConfirmDelete,
+                                                onCancel,
+                                                onConfirmEdit,
+                                              }) => {
   const popupRef = useRef<HTMLDivElement>(null);
 
   usePopupClose(popupRef, onCancel);
 
   return (
     <div className={styles.dropdown} ref={popupRef}>
-      {onConfirmEdit && <button className={styles.menuItem} onClick={onConfirmEdit}>Edit</button>}
-      <button className={styles.menuItem} onClick={onConfirmDelete}>Delete</button>
+      {onConfirmEdit && (
+        <button
+          className={styles.menuItem}
+          onClick={() => {
+            onConfirmEdit();
+            onCancel();
+          }}
+        >
+          <Pencil size={16}/> Edit
+        </button>
+      )}
+      <button
+        className={styles.menuItem}
+        onClick={() => {
+          onConfirmDelete();
+          onCancel();
+        }}
+      >
+        <Trash2 size={16}/> Delete
+      </button>
     </div>
   );
 };
