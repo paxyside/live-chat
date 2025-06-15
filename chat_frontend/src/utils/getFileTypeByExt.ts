132b786aab13f3fefbe564a16cff220a31e4bcf7
Pavel Grebnev <pavel.grebnev@rocknblock.io>
2025-06-15 18:08:46 +0300

    add heic file ext

--- chat_frontend/src/utils/getFileTypeByExt.ts
+++ chat_frontend/src/utils/getFileTypeByExt.ts
@@ -10,7 +10,7 @@ export type FileTypes = typeof FileTypes[keyof typeof FileTypes];
 export function getFileTypeByExtension(url: string): FileTypes {
   const ext = url.split('.').pop()?.toLowerCase();
   if (!ext) return FileTypes.OTHER;
-  if (["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg"].includes(ext)) return FileTypes.IMAGE;
+  if (["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg", "heic"].includes(ext)) return FileTypes.IMAGE;
   if (["mp4", "webm", "ogg", "mov", "avi"].includes(ext)) return FileTypes.VIDEO;
   if (["mp3", "wav", "ogg", "aac", "flac"].includes(ext)) return FileTypes.AUDIO;
   return FileTypes.OTHER;
